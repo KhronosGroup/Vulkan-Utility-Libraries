6999ef9b82191d4824cd105aba1f5f2433b4ccb6
Juan Ramos <juan@lunarg.com>
2023-08-15 16:20:56 -0600

    Add testing for vk_enum_string_helper.h
    Use magic_enum to test vk_enum_string_helper.h
    
    closes #46

--- tests/generated/vk_enum_string_helper.cpp
+++ tests/generated/vk_enum_string_helper.cpp
@@ -5,5 +5,72 @@
 // SPDX-License-Identifier: Apache-2.0
 #include <vulkan/vk_enum_string_helper.h>
 
-// TODO: Add actual testing
-int main() {}
+#include <magic_enum.hpp>
+
+#include <gtest/gtest.h>
+
+TEST(vk_enum_string_helper, string_VkResult) {
+    constexpr auto values = magic_enum::enum_values<VkResult>();
+    for (auto val : values) {
+        auto magic_str = magic_enum::enum_name(val);
+
+        auto str = string_VkResult(val);
+
+        EXPECT_STREQ(magic_str.data(), str);
+    }
+}
+
+TEST(vk_enum_string_helper, string_VkStructureType) {
+    constexpr auto values = magic_enum::enum_values<VkStructureType>();
+    for (auto val : values) {
+        auto magic_str = magic_enum::enum_name(val);
+
+        auto str = string_VkStructureType(val);
+
+        EXPECT_STREQ(magic_str.data(), str);
+    }
+}
+
+TEST(vk_enum_string_helper, string_VkPipelineCacheHeaderVersion) {
+    constexpr auto values = magic_enum::enum_values<VkPipelineCacheHeaderVersion>();
+    for (auto val : values) {
+        auto magic_str = magic_enum::enum_name(val);
+
+        auto str = string_VkPipelineCacheHeaderVersion(val);
+
+        EXPECT_STREQ(magic_str.data(), str);
+    }
+}
+
+TEST(vk_enum_string_helper, string_VkImageLayout) {
+    constexpr auto values = magic_enum::enum_values<VkImageLayout>();
+    for (auto val : values) {
+        auto magic_str = magic_enum::enum_name(val);
+
+        auto str = string_VkImageLayout(val);
+
+        EXPECT_STREQ(magic_str.data(), str);
+    }
+}
+
+TEST(vk_enum_string_helper, string_VkObjectType) {
+    constexpr auto values = magic_enum::enum_values<VkObjectType>();
+    for (auto val : values) {
+        auto magic_str = magic_enum::enum_name(val);
+
+        auto str = string_VkObjectType(val);
+
+        EXPECT_STREQ(magic_str.data(), str);
+    }
+}
+
+TEST(vk_enum_string_helper, string_VkFormat) {
+    constexpr auto values = magic_enum::enum_values<VkFormat>();
+    for (auto val : values) {
+        auto magic_str = magic_enum::enum_name(val);
+
+        auto str = string_VkFormat(val);
+
+        EXPECT_STREQ(magic_str.data(), str);
+    }
+}
