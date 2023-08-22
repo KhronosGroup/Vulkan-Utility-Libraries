2bd0e0d43884f1d655d66c2195539834a113f850
Charles Giessen <charles@lunarg.com>
2023-08-22 14:32:01 -0600

    util: Fixup and add Dispatch Table header
    Fixes the vk_layer_dispatch_table.h header file so that they can be used
    in other projects. The contents of this header and
     vk_dispatch_table_helper.h have been moved into a new header
     vul_dispatch_table.h.
    
    The structs VulDeviceDispatchTable and VulInstanceDispatchTable
    struct contain function pointers for the device and instance, respectively.
    
    The functions vul_init_device_dispatch_table and
    vul_init_instance_dispatch_table fill out the aforementioned structs,
    making the task of setting up the disptach table in a layer much simpler.

--- tests/add_subdirectory/client.c
+++ tests/add_subdirectory/client.c
@@ -9,9 +9,9 @@ VkBool32 foobar() {
     VlLayerSettingSet layerSettingSet = VK_NULL_HANDLE;
     vlCreateLayerSettingSet("VK_LAYER_LUNARG_test", NULL, NULL, NULL, &layerSettingSet);
 
-	VkBool32 result = vlHasLayerSetting(layerSettingSet, "setting_key") ? VK_TRUE : VK_FALSE;
+    VkBool32 result = vlHasLayerSetting(layerSettingSet, "setting_key") ? VK_TRUE : VK_FALSE;
 
     vlDestroyLayerSettingSet(layerSettingSet, NULL);
 
-	return result;
+    return result;
 }
